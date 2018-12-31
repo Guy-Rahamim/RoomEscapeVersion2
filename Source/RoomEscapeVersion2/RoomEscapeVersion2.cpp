0055f34728b18d715eaf4458201b6e37a7448464
GameBrew <guy.danino5@gmail.com>
2018-12-31 17:00:49 +0200

    Initial Commit

---
+++ Source/RoomEscapeVersion2/RoomEscapeVersion2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RoomEscapeVersion2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RoomEscapeVersion2, "RoomEscapeVersion2" );
